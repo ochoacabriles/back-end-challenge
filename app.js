812e52011749f5f33c2743218540404a0a5c16f7
Rafael <ochoacabriles@gmail.com>
2019-07-30 11:55:17 -0500

    updated from jade to pug

--- app.js
+++ app.js
@@ -34,7 +34,7 @@ mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true })
 
 // Express Setup
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+app.set('view engine', 'pug');
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
